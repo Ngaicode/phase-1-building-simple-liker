703d2b31aa24cd3f0536a29dbb653152d3103d5e
Ngaicode <mjombakevin376@gmail.com>
2022-06-18 17:57:53 +0300

    commit tree ade9b6c9635fe62a2c9425ec75195f6b25899425

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
+// selecting the like elements
+const hearts = document.querySelectorAll(".like-glyph");
 
+function callbackWhenLiked(e) {
+  const heart = e.target;
+  mimicServerCall("http://mimicServer.example.com")
+    .then(function () {
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function (error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() => (modal.className = "hidden"), 3000);
+    });
+}
 
-
+// adding an a event listener to the heart shapes and attatching a  handler function to it
+for (const loveShape of hearts) {
+  loveShape.addEventListener("click", callbackWhenLiked);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
